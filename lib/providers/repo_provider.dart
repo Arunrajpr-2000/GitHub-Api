93697203ccbcccae83bcacac7857a5df20b53331
Arunrajpr-2000 <arunrajpr3@gmail.com>
2023-02-06 18:41:01 +0530

    second

--- lib/providers/repo_provider.dart
+++ lib/providers/repo_provider.dart
@@ -5,8 +5,12 @@ import 'package:github_api/model/repo_model.dart';
 import 'package:github_api/utils/api.dart';
 import 'package:http/http.dart' as http;
 
+// Map mapResponse = {};
+// List listResponse = [];
+// dynamic responseData;
+
 class RepoProvider {
-  List<RepoModel>? user;
+  RepoModel? user;
 
   Future<void> getRepo(String username) async {
     final url = '${Api.api}/users/${username}/repos';
@@ -15,15 +19,16 @@ class RepoProvider {
     try {
       final response = await http.get(Uri.parse(url),
           headers: {'Authorization': 'token ${Api.token}'});
-      //log(response.body);
 
       final responseData = json.decode(response.body);
+
+      log(responseData[0]['name'].toString());
 
       user = RepoModel(
         reponame: responseData['name'],
         starcount: responseData['stargazers_count'],
         forkscount: responseData['forks_count'],
-      ) as List<RepoModel>;
+      );
 
       // notifyListeners();
     } catch (e) {
